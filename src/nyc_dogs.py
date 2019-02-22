42c2f7f56cdbfda03f4c39a792628df9380514fc
Joshua Reed <joshua.reed@algolux.com>
2019-02-22 15:52:44 -0500

    create a counter implementation

--- src/nyc_dogs.py
+++ src/nyc_dogs.py
@@ -1,13 +1,29 @@
 import csv
 import logging
 import pathlib
-from typing import Sequence, MutableMapping
+from typing import Sequence, MutableMapping, Iterable, Mapping
 
 import attr
 from tornado import httpclient, ioloop, web
 
 
 logger = logging.getLogger(__name__)
+
+
+def ilen(xs: Iterable) -> int:
+    """
+    Count number of instances in any terminator iterable.
+
+    :param xs: any iterable, don't pass infinite iterables here.
+    :returns: number of instances
+
+    This does not allocate any temporary memory just to get length.
+    Note: this consumes generators.  Copy to list if you need that.
+    """
+    acc = 0
+    for _ in xs:
+        acc += 1
+    return acc
 
 
 @attr.s
@@ -18,6 +34,11 @@ class DataCounter:
     :attribute _records: list of mappings of dog data
     """
     _records: Sequence[MutableMapping[str, str]] = attr.ib(repr=False)
+    _column_names: Sequence[str] = attr.ib()
+
+    @_column_names.default
+    def _read_column_names(self):
+        return list(self._records[0].keys())
 
     @classmethod
     def load_csv(cls, filepath: pathlib.Path) -> 'DataCounter':
@@ -31,7 +52,22 @@ class DataCounter:
 
     @property
     def columns(self) -> Sequence[str]:
-        return list(self._records[0].keys())
+        """
+        Return the column names as a sequence.
+        """
+        return self._column_names
+
+    def occurances(self, values: MutableMapping[str, str]) -> int:
+        """
+        Count occurances of the ``value`` in ``column, case-insentively.
+
+        :param column: column name
+        :param value: value to compare with
+        :returns: count of instances in record data
+        """
+        keys, vals = zip(*((k.lower(), v.lower()) for k, v in values.items()))
+        series = (tuple(rec[k].lower() for k in keys) for rec in self._records)
+        return ilen(tup for tup in series if tup == vals)
 
 
 class CountHandler(web.RequestHandler):
@@ -40,7 +76,16 @@ class CountHandler(web.RequestHandler):
         self.counter = counter
 
     def get(self) -> None:
-        self.write({"hello": self.counter.columns})
+        raw_args = self.request.arguments
+        args = {k.lower(): v[-1].decode('utf-8') for k, v in raw_args.items()}
+        logging.debug("got count args: %s", args)
+        unknown = set(args).difference(self.counter.columns)
+        if unknown:
+            self.write({"unknown fields": sorted(unknown)})
+            self.set_status(400)
+            return
+        self.write({"count": self.counter.occurances(args)})
+        self.set_header('Content-Type', 'application/json')
 
 
 def make_application(data_path) -> web.Application:
